3c9c219869ff8c73a709320bb2befde59662320c
y12rf <1354810617@qq.com>
2025-05-12 18:39:09 +0800

    Update mhs-autobuy.meta.js

--- mhs-autobuy.meta.js
+++ mhs-autobuy.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        米画师自动购买橱窗脚本
 // @namespace   https://github.com/y12rf/mhs-autobuy
-// @version     1.9.3
+// @version     1.9.4
 // @description 头部文件，仅用于 Tampermonkey 检查更新
 // @author      和川 & ChatGPT
 // @match       *://www.mihuashi.com/stalls/*
