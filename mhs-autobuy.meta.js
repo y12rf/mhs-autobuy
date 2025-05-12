cdf31daa0726db639b769939b425835ce030a3b8
y12rf <1354810617@qq.com>
2025-05-12 18:30:28 +0800

    Update mhs-autobuy.meta.js

--- mhs-autobuy.meta.js
+++ mhs-autobuy.meta.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        米画师自动购买橱窗脚本 (Stealth)
+// @name        米画师自动购买橱窗脚本
 // @namespace   https://github.com/y12rf/mhs-autobuy
 // @version     1.9.3
 // @description 头部文件，仅用于 Tampermonkey 检查更新
